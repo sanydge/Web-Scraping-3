1663a1abe9aa18c25bb91b3fc2e153c0285983a8
sanydge <sanydge@gmail.com>
2020-03-01 15:32:37 +0200

    finished version

--- imdb/settings.py
+++ imdb/settings.py
@@ -13,6 +13,7 @@ BOT_NAME = 'imdb'
 
 SPIDER_MODULES = ['imdb.spiders']
 NEWSPIDER_MODULE = 'imdb.spiders'
+FEED_EXPORT_ENCODING = 'utf-8'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
--- imdb/spiders/best_movies.py
+++ imdb/spiders/best_movies.py
@@ -7,15 +7,19 @@ from scrapy.spiders import CrawlSpider, Rule
 class BestMoviesSpider(CrawlSpider):
     name = 'best_movies'
     allowed_domains = ['imdb.com']
-    start_urls = ['http://imdb.com/']
+    start_urls = ['https://www.imdb.com/search/title/?genres=drama&groups=top_250&sort=user_rating,desc']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths="//h3[@class='lister-item-header']/a"), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths="(//a[@class='lister-page-next next-page'])[2]"))
     )
 
     def parse_item(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+        yield{
+            'title': response.xpath("//div[@class='title_wrapper']/h1/text()").get(),
+            'year': response.xpath("//span[@id='titleYear']/a/text()").get(),
+            'duration': response.xpath("normalize-space((//time)[1]/text())").get(),
+            'genre': response.xpath("//div[@class='subtext']/a[1]/text()").get(),
+            'rating': response.xpath("//span[@itemprop='ratingValue']/text()").get(),
+            'movie_url': response.url,
+        }
